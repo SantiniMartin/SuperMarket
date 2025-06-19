d582a8c4fb948e30f54ca0a70d96a6ab38df3bf2
Elias <eliasmorales.21@hotmail.com>
2025-06-18 23:49:48 -0300

    [FIX] GIT gg

--- app/(tabs)/stores.tsx
+++ app/(tabs)/stores.tsx
@@ -1,17 +1,5 @@
 import { View, Text } from "react-native";
 import Store from "@/components/store/store";
 export default function TiendasScreen() {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  return (
-    <View>
-      <Text>Pantalla de Tiendas</Text>
-    </View>
-  );
-} 
-=======
-=======
->>>>>>> Stashed changes
   return <Store></Store>;
 }
->>>>>>> Stashed changes
